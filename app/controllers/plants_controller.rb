0a0a6fd03e2aabfb879da579953fe280385d62a7
Daniel Rice <danielrice5627@gmail.com>
2023-05-19 21:05:51 -0700

    commit tree b2fb602070f8c65b807d9dcfb4585e275b863ce1

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,27 @@
+class PlantsController < ApplicationController
+    wrap_parameters format: []
+
+    #GET /plants
+    def index
+        render json: Plant.all, status: :ok
+    end
+
+    #GET /plants/:id
+    def show
+        plant = Plant.find(params[:id])
+        render json: plant, status: :ok
+    end
+
+    #POST /plants
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
